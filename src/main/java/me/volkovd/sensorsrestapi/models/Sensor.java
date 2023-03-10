9e63d5b6f78b7c7fb81fa6207c75d23963723de8
Volkov Daniil <volkov27daniil@yandex.ru>
2023-03-10 20:42:21 +0300

    feat(sensor): Create Sensor entity

---
+++ src/main/java/me/volkovd/sensorsrestapi/models/Sensor.java
@@ -0,0 +1,65 @@
+package me.volkovd.sensorsrestapi.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Table(name = "sensor")
+public class Sensor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotEmpty
+    @Size(min = 3, max = 30)
+    private String name;
+
+    public Sensor() {
+    }
+
+    public Sensor(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sensor sensor = (Sensor) o;
+        return id == sensor.id && Objects.equals(name, sensor.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Sensor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
