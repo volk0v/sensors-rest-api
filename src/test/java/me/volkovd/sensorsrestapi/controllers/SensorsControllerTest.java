f9d493fc22af162786abfdf21f5036c33be161f5
Volkov Daniil <volkov27daniil@yandex.ru>
2023-03-10 22:07:53 +0300

    test(sensors-contr): Add test for register(...) with correct request
    Given correct request,
    When POST '/sensors/registration',
    Then return status ok (200) and SensorsService#save(...) was invoked.

--- src/test/java/me/volkovd/sensorsrestapi/controllers/SensorsControllerTest.java
+++ src/test/java/me/volkovd/sensorsrestapi/controllers/SensorsControllerTest.java
@@ -1,21 +1,28 @@
 package me.volkovd.sensorsrestapi.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import me.volkovd.sensorsrestapi.dto.SensorDTO;
 import me.volkovd.sensorsrestapi.mapper.SensorMapper;
 import me.volkovd.sensorsrestapi.models.Sensor;
 import me.volkovd.sensorsrestapi.services.SensorsService;
 import me.volkovd.sensorsrestapi.validators.SensorValidator;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SensorsController.class)
 @ExtendWith(SpringExtension.class)
@@ -40,6 +47,19 @@ class SensorsControllerTest {
             SensorDTO sensorDTO = invocationOnMock.getArgument(0);
             return new Sensor(sensorDTO.getName());
         });
+    }
+
+    @Test
+    public void givenCorrectRequest_whenPostRegistration_thenSensorSaved() throws Exception {
+        SensorDTO sensorDTO = new SensorDTO("test-sensor");
+        String requestBody = new ObjectMapper().writeValueAsString(sensorDTO);
+
+        mvc.perform(post("/sensors/registration")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk());
+
+        verify(service, times(1)).save(any());
     }
 
 }
