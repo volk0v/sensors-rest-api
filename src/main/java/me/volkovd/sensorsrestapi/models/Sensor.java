45d908019517d0a33776ff733791d7c00107ab1a
Volkov Daniil <volkov27daniil@yandex.ru>
2023-03-10 20:44:43 +0300

    feat(sensor): Add validation error's messages

--- src/main/java/me/volkovd/sensorsrestapi/models/Sensor.java
+++ src/main/java/me/volkovd/sensorsrestapi/models/Sensor.java
@@ -14,8 +14,8 @@ public class Sensor {
     @Column(name = "id")
     private int id;
 
-    @NotEmpty
-    @Size(min = 3, max = 30)
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 3, max = 30, message = "Name should be between 3 and 30 characters")
     private String name;
 
     public Sensor() {
