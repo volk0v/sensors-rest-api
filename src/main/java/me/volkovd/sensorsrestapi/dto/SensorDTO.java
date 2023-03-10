1433122e3b6a206e0ea468f5c7597d2b1a57d07f
Volkov Daniil <volkov27daniil@yandex.ru>
2023-03-10 21:08:38 +0300

    feat(sensor): Add validation in SensorDTO class

--- src/main/java/me/volkovd/sensorsrestapi/dto/SensorDTO.java
+++ src/main/java/me/volkovd/sensorsrestapi/dto/SensorDTO.java
@@ -1,9 +1,13 @@
 package me.volkovd.sensorsrestapi.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class SensorDTO {
 
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 3, max = 30, message = "Name should be between 3 and 30 characters")
     private String name;
 
     public SensorDTO(String name) {
