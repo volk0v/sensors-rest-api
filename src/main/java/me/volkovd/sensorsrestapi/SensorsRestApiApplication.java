9fa942a6559f09747b55d7645d1f3537ef583055
Volkov Daniil <volkov27daniil@yandex.ru>
2023-03-10 20:35:56 +0300

    Initial commit

---
+++ src/main/java/me/volkovd/sensorsrestapi/SensorsRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.volkovd.sensorsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorsRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorsRestApiApplication.class, args);
+    }
+
+}
