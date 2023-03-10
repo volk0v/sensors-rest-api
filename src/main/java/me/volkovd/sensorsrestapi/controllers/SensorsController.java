0840039094b37e3bfa665b0612086266552e165a
Volkov Daniil <volkov27daniil@yandex.ru>
2023-03-10 21:31:38 +0300

    feat(sensors-contr): Create SensorsController class

---
+++ src/main/java/me/volkovd/sensorsrestapi/controllers/SensorsController.java
@@ -0,0 +1,9 @@
+package me.volkovd.sensorsrestapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sensors")
+public class SensorsController {
+}
