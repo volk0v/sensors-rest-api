c45ab611cc79cacb6029184f4512e8f99fc3c877
Volkov Daniil <volkov27daniil@yandex.ru>
2023-03-10 21:57:29 +0300

    feat(sensor): Add empty constructor to SensorDTO class

--- src/main/java/me/volkovd/sensorsrestapi/dto/SensorDTO.java
+++ src/main/java/me/volkovd/sensorsrestapi/dto/SensorDTO.java
@@ -10,6 +10,9 @@ public class SensorDTO {
     @Size(min = 3, max = 30, message = "Name should be between 3 and 30 characters")
     private String name;
 
+    public SensorDTO() {
+    }
+
     public SensorDTO(String name) {
         this.name = name;
     }
