6522ffdb51b4cc3c95eb9b2459bbb907f237f4c1
Volkov Daniil <volkov27daniil@yandex.ru>
2023-05-01 18:01:08 +0300

    feat(measurement): Create Measurement entity
    Create new entity based on 'measurement' table.

---
+++ src/main/java/me/volkovd/sensorsrestapi/models/Measurement.java
@@ -0,0 +1,88 @@
+package me.volkovd.sensorsrestapi.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "measurement")
+public class Measurement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "value")
+    private float value;
+
+    @Column(name = "raining")
+    private boolean raining;
+
+    @ManyToOne
+    private Sensor sensor;
+
+    public Measurement() {
+    }
+
+    public Measurement(float value, boolean raining, Sensor sensor) {
+        this.value = value;
+        this.raining = raining;
+        this.sensor = sensor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(boolean raining) {
+        this.raining = raining;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Measurement that = (Measurement) o;
+        return id == that.id && Float.compare(that.value, value) == 0 && raining == that.raining && Objects.equals(sensor, that.sensor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value, raining, sensor);
+    }
+
+    @Override
+    public String toString() {
+        return "Measurement{" +
+                "id=" + id +
+                ", value=" + value +
+                ", raining=" + raining +
+                ", sensor=" + sensor +
+                '}';
+    }
+
+}
