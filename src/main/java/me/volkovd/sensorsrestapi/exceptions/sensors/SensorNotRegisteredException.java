c34f331445953fac3d792845cf8b96cc82cce7d2
Volkov Daniil <volkov27daniil@yandex.ru>
2023-05-01 18:55:16 +0300

    refactor(exceptions): Change SensorNotRegistered... to extend Validating...
    Change SensorNotRegisteredException to extend ValidatingException instead of
    RuntimeException.
    This is done due to remove boilerplate code in 'validating' exceptions.

--- src/main/java/me/volkovd/sensorsrestapi/exceptions/sensors/SensorNotRegisteredException.java
+++ src/main/java/me/volkovd/sensorsrestapi/exceptions/sensors/SensorNotRegisteredException.java
@@ -1,36 +1,16 @@
 package me.volkovd.sensorsrestapi.exceptions.sensors;
 
-import me.volkovd.sensorsrestapi.utils.FieldErrors;
+import me.volkovd.sensorsrestapi.exceptions.ValidatingException;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class SensorNotRegisteredException extends RuntimeException {
-
-    private BindingResult bindingResult;
+public class SensorNotRegisteredException extends ValidatingException {
 
     public SensorNotRegisteredException(String message) {
         super(message);
     }
 
     public SensorNotRegisteredException(BindingResult bindingResult) {
-        this.bindingResult = bindingResult;
-    }
-
-    public List<FieldErrors> getErrors() {
-        List<FieldErrors> errors = new ArrayList<>();
-
-        for (FieldError error : bindingResult.getFieldErrors()) {
-            errors.add(new FieldErrors(error.getField(), error.getDefaultMessage()));
-        }
-
-        return errors;
-    }
-
-    public BindingResult getBindingResult() {
-        return bindingResult;
+        super(bindingResult);
     }
 
 }
