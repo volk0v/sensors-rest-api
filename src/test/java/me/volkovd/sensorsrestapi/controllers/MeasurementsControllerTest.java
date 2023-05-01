dd09730a06cfb90cd285e37444c2299609cb1b27
Volkov Daniil <volkov27daniil@yandex.ru>
2023-05-01 19:41:21 +0300

    test(measurements-contr): Add test for add(...) with correct request
    Given correct request,
    When POST '/measurements/add',
    Then return status ok (200), MeasurementsService#save(...) was invoked

--- src/test/java/me/volkovd/sensorsrestapi/controllers/MeasurementsControllerTest.java
+++ src/test/java/me/volkovd/sensorsrestapi/controllers/MeasurementsControllerTest.java
@@ -1,22 +1,30 @@
 package me.volkovd.sensorsrestapi.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import me.volkovd.sensorsrestapi.dto.MeasurementDTO;
+import me.volkovd.sensorsrestapi.dto.SensorDTO;
 import me.volkovd.sensorsrestapi.mapper.MeasurementMapper;
 import me.volkovd.sensorsrestapi.models.Measurement;
 import me.volkovd.sensorsrestapi.models.Sensor;
 import me.volkovd.sensorsrestapi.services.MeasurementsService;
 import me.volkovd.sensorsrestapi.validators.MeasurementValidator;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MeasurementsController.class)
 @ExtendWith(SpringExtension.class)
@@ -41,6 +49,19 @@ class MeasurementsControllerTest {
             MeasurementDTO dto = invocationOnMock.getArgument(0);
             return new Measurement(dto.getValue(), dto.isRaining(), new Sensor(dto.getSensor().getName()));
         });
+    }
+
+    @Test
+    public void givenCorrectRequest_whenPostAdd_thenSaved() throws Exception {
+        MeasurementDTO dto = new MeasurementDTO(24.7f, false, new SensorDTO("Sensor name"));
+        String requestBody = new ObjectMapper().writeValueAsString(dto);
+
+        mvc.perform(post("/measurements/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk());
+
+        verify(service, times(1)).save(any());
     }
 
 }
