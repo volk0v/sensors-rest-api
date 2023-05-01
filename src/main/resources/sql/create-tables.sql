428856a91af181387daa05302097311133e44cd6
Volkov Daniil <volkov27daniil@yandex.ru>
2023-05-01 19:11:54 +0300

    feat(sql): Alter 'measurement' table to add 'created_at' column

--- src/main/resources/sql/create-tables.sql
+++ src/main/resources/sql/create-tables.sql
@@ -6,8 +6,9 @@ CREATE TABLE IF NOT EXISTS sensor
 
 CREATE TABLE IF NOT EXISTS measurement
 (
-    id        INT AUTO_INCREMENT PRIMARY KEY,
-    value     FLOAT NOT NULL,
-    raining   BOOL  NOT NULL,
-    sensor_id INT REFERENCES sensor (id)
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    value      FLOAT     NOT NULL,
+    raining    BOOL      NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    sensor_id  INT REFERENCES sensor (id)
 );
