fdcb21ff00d29c1450c797814c38055c271e01e4
Volkov Daniil <volkov27daniil@yandex.ru>
2023-05-01 18:46:55 +0300

    refactor(measurements-contr): Create controller
    Create empty MeasurementController.

---
+++ src/main/java/me/volkovd/sensorsrestapi/controllers/MeasurementsController.java
@@ -0,0 +1,10 @@
+package me.volkovd.sensorsrestapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/measurements")
+public class MeasurementsController {
+
+}
