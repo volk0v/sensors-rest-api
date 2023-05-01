a2f97507c960c7fa0866ed5ca75c71a0101df46f
Volkov Daniil <volkov27daniil@yandex.ru>
2023-05-01 17:57:28 +0300

    feat(sql): Add 'measurement' table script
    Change create-tables.sql script to creating 'measurement' table for
    Measurement entity.

--- src/main/resources/sql/create-tables.sql
+++ src/main/resources/sql/create-tables.sql
@@ -3,3 +3,11 @@ CREATE TABLE IF NOT EXISTS sensor
     id   INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS measurement
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    value     FLOAT NOT NULL,
+    raining   BOOL  NOT NULL,
+    sensor_id INT REFERENCES sensor (id)
+);
