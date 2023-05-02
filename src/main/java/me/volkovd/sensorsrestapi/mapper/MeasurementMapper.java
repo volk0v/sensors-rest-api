d58ce212ae701867d9af3ae4471e6003cafa3545
Volkov Daniil <volkov27daniil@yandex.ru>
2023-05-02 20:26:14 +0300

    feat(measurements-mapper): Add mapping methods for lists
    Create toListOfDTOs(...) and toListOfModels(...) methods which accept list
    of objects and returns list of DTOs or models.

--- src/main/java/me/volkovd/sensorsrestapi/mapper/MeasurementMapper.java
+++ src/main/java/me/volkovd/sensorsrestapi/mapper/MeasurementMapper.java
@@ -5,6 +5,9 @@ import me.volkovd.sensorsrestapi.models.Measurement;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class MeasurementMapper {
 
@@ -18,8 +21,28 @@ public class MeasurementMapper {
         return modelMapper.map(measurement, MeasurementDTO.class);
     }
 
+    public List<MeasurementDTO> toListOfDTOs(List<Measurement> measurements) {
+        List<MeasurementDTO> result = new ArrayList<>();
+
+        for (Measurement measurement : measurements) {
+            result.add(toDTO(measurement));
+        }
+
+        return result;
+    }
+
     public Measurement toModel(MeasurementDTO measurementDTO) {
         return modelMapper.map(measurementDTO, Measurement.class);
+    }
+
+    public List<Measurement> toListOfModels(List<MeasurementDTO> measurementsDTOs) {
+        List<Measurement> result = new ArrayList<>();
+
+        for (MeasurementDTO measurementDTO : measurementsDTOs) {
+            result.add(toModel(measurementDTO));
+        }
+
+        return result;
     }
 
 }
